84ffc8a13474dc12dd37aa6c2fadf903099fa88a
stef2georg <stefan.georgiev91@gmail.com>
2020-05-27 13:55:26 +0300

    Add main classes to client and server

---
+++ grpc.intro/client/src/test/java/com/epam/grpc/intro/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.grpc.intro.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- grpc.intro/server/src/test/java/com/epam/grpc/intro/server/ServerApplicationTests.java
+++ grpc.intro/server/src/test/java/com/epam/grpc/intro/server/ServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.epam.grpc.intro;
+package com.epam.grpc.intro.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+public class ServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
